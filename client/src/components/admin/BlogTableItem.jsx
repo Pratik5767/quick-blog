4fc8f26edc2e73f2120f21ad182cde9876b26067
Pratik5676 <pratikvsalunkhe924@gmail.com>
2025-07-04 18:04:07 +0530

    created the comments and bloglist component of the admin page

--- client/src/components/admin/BlogTableItem.jsx
+++ client/src/components/admin/BlogTableItem.jsx
@@ -15,13 +15,13 @@ const BlogTableItem = ({ blog, fetchBlogs, index }) => {
            
             <td className="px-2 py-4 max-sm:hidden">
                 <p className={`${blog.isPublished ? 'text-green-600' : 'text-orange-700'}`}>
-                    {blog.isPublished ? 'Published' : 'UnPublished'}
+                    {blog.isPublished ? 'Published' : 'UnPublish'}
                 </p>
             </td>
 
             <td className="px-2 py-4 flex text-xs gap-3">
                 <button className="border px-2 py-0.5 mt-1 rounded cursor-pointer">
-                    {blog.isPublished ? 'UnPublished' : 'Published'}
+                    {blog.isPublished ? 'UnPublish' : 'Published'}
                 </button>
 
                 <img src={assets.cross_icon} className="w-8 hover:scale-110 transition-all cursor-pointer" alt="cross" />
